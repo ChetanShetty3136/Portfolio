241af0c500cd9afbaf334138ea6ea0c41aba7088
Chetan <chetan.shekar.ss@gmail.com>
2026-06-30 16:15:58 +0530

    full code

--- javascript.js
+++ javascript.js
@@ -3,28 +3,38 @@ const greeting = document.getElementById("greeting");
 const hour = new Date().getHours();
 
 if (hour < 12) {
-    greeting.innerHTML = "Good Morning, I'm";
+    greeting.innerHTML = "Good Morning ";
 }
 else if (hour < 18) {
-    greeting.innerHTML = "Good Afternoon, I'm";
+    greeting.innerHTML = "Good Afternoon ";
 }
 else {
-    greeting.innerHTML = "Good Evening, I'm";
+    greeting.innerHTML = "Good Evening ";
 }
-const text = "Aspiring Full Stack Developer";
-let index = 0;
+const text = "Computer Science Engineering Student Aspiring Full Stack Developer";
+let index = 36;
+let deleting = false;
 
 function typeWriter() {
-
-    if(index < text.length){
-
-        document.getElementById("typing").innerHTML += text.charAt(index);
-
+    const pr = document.getElementById("typing");
+    if(!deleting) {
+        pr.innerHTML = text.substring(36,index+1);
         index++;
-
-        setTimeout(typeWriter, 100);
+        if (index === text.length) {
+            deleting = true;
+            setTimeout(typeWriter, 1000); 
+            return;
+        }
     }
-
+    else {
+        pr.innerHTML = text.substring(36,index - 1);   
+        index--;
+        if (index === 36) {      
+        deleting = false;   
+        }
+    }setTimeout(typeWriter, 100); 
 }
+    
+
 
 typeWriter();
